ee85f18aa84ecb8e26d9a8a08ee7b2e545140cdd
Kabirou ALASSANE <kabirou2001@gmail.com>
2023-06-16 19:18:56 +0000

    App: Init Dashboard Pages

--- App/urls.py
+++ App/urls.py
@@ -3,4 +3,9 @@ from . import views
 
 urlpatterns = [
     path('', views.default, name='default'),
+    path('teachers', views.teachers, name='teachers'),
+    path('shedules', views.shedules, name='shedules'),
+    path('courses', views.cours, name='courses'),
+    path('classrooms', views.salles, name='classrooms'),
+    path('helps', views.aides, name='helps'),
 ]
--- App/views.py
+++ App/views.py
@@ -8,3 +8,38 @@ def default(request):
     context = {}
     
     return render(request, 'app/default.html', context)
+
+@login_required
+def teachers(request):
+    
+    context = {}
+    
+    return render(request, 'app/teachers.html', context)
+
+@login_required
+def cours(request):
+    
+    context = {}
+    
+    return render(request, 'app/courses.html', context)
+
+@login_required
+def salles(request):
+    
+    context = {}
+    
+    return render(request, 'app/classrooms.html', context)
+
+@login_required
+def shedules(request):
+    
+    context = {}
+    
+    return render(request, 'app/shedules.html', context)
+
+@login_required
+def aides(request):
+    
+    context = {}
+    
+    return render(request, 'app/helps.html', context)
