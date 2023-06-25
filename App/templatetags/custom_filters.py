145d5906ea67203f30ba8f24ebb32294d36dd222
Kabirou ALASSANE <kabirou2001@gmail.com>
2023-06-25 15:52:04 +0100

    Affichage sur la page des cours du temps écoulé d'une matiere sous format pourcentage

--- App/templatetags/custom_filters.py
+++ App/templatetags/custom_filters.py
@@ -2,6 +2,7 @@ from django import template
 from django.db.models import Sum
 from App.models import Timetable
 from django.db import models
+from datetime import timedelta
 
 
 register = template.Library()
@@ -11,4 +12,9 @@ def calculate_total_hours(subject_id):
     total_hours = Timetable.objects.filter(subject_id=subject_id).aggregate(
         total=Sum(models.F('end_time') - models.F('start_time')))['total']
     return total_hours if total_hours is not None else 0
+
+@register.filter
+def calculate_total_percentage(subject):
+    total_hours = (calculate_total_hours(subject.id) * 100) / timedelta(hours=subject.total_time)
+    return total_hours
 
